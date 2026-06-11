52546b94fb178151dd03036151fec5f8bcd63fb7
jayaeducationgroup <75846860+smitscolar@users.noreply.github.com>
2026-06-11 21:56:25 +0700

    Switch password hashing to pbkdf2_sha256

--- backend/app/security.py
+++ backend/app/security.py
@@ -1,14 +1,12 @@
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(
-    schemes=["bcrypt"],
+    schemes=["pbkdf2_sha256"],
     deprecated="auto"
 )
 
 def hash_password(password: str):
-    password = password[:72]
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str):
-    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
