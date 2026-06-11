4617705c2872a4af18c7e72d1ec27dc83c20aefd
jayaeducationgroup <75846860+smitscolar@users.noreply.github.com>
2026-06-11 15:37:47 +0700

    Add basic user registration endpoint

--- backend/app/users.py
+++ backend/app/users.py
@@ -2,10 +2,24 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+fake_users = []
+
 @router.get("/users")
 def get_users():
+    return fake_users
+
+@router.post("/register")
+def register(username: str, email: str):
+    user = {
+        "username": username,
+        "email": email
+    }
+
+    fake_users.append(user)
+
     return {
-        "message": "IBRAAI User Service"
+        "message": "User registered",
+        "user": user
     }
 
 @router.get("/users/profile")
