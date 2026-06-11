4fc17ba7d4dfc892a36e0e65fe95fb5b048c5934
jayaeducationgroup <75846860+smitscolar@users.noreply.github.com>
2026-06-12 03:45:46 +0700

    Add project model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text
+
 from app.database import Base
 
 
@@ -10,3 +11,12 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
     role = Column(String, default="user")
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(Text)
+    status = Column(String, default="active")
