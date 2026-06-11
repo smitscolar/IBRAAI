21a55af567e1609d15e618c43039488770d0de92
jayaeducationgroup <75846860+smitscolar@users.noreply.github.com>
2026-06-11 15:54:04 +0700

    Update auth.py

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,20 +1,22 @@
 from jose import jwt
 from datetime import datetime, timedelta
 
-SECRET_KEY = "ibraai-secret-key"
-ALGORITHM = "HS256"
+from app.config import (
+    SECRET_KEY,
+    ALGORITHM,
+    ACCESS_TOKEN_EXPIRE_MINUTES
+)
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(hours=1)
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
 
     to_encode.update({
         "exp": expire
     })
 
     return jwt.encode(
-        to_encode,
-        SECRET_KEY,
-        algorithm=ALGORITHM
-    )
+        to_encode
