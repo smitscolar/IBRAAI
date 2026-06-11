84e8e910ce3ee31e4ec1a35d9a2c690b41a0cc81
jayaeducationgroup <75846860+smitscolar@users.noreply.github.com>
2026-06-12 00:00:45 +0700

    Add JWT token verification

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,11 +1,16 @@
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import datetime, timedelta
+from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
 from app.config import (
     SECRET_KEY,
     ALGORITHM,
     ACCESS_TOKEN_EXPIRE_MINUTES
 )
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -19,7 +24,32 @@ def create_access_token(data: dict):
     })
 
     return jwt.encode(
-    to_encode,
-    SECRET_KEY,
-    algorithm=ALGORITHM
-)
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        username = payload.get("sub")
+
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication token"
+            )
+
+        return username
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication token"
+        )
