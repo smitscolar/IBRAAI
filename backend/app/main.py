1fa202808dd09db622ceccb031ef34509509f559
jayaeducationgroup <75846860+smitscolar@users.noreply.github.com>
2026-06-11 17:00:12 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from app.users import router as users_router
+from app.database import engine
+from app.models import Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="IBRAAI API",
